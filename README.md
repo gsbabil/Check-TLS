a969ce12369081de9dcf20fabac7f834f8f7e105
Babil Golam Sarwar <gsbabil@gmail.com>
2015-12-26 17:54:08 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # check-tls
-A simple OpenSSL wrapper to check server-side TLS capabilities
+A simple OpenSSL wrapper to check server-side TLS capabilities.
+
+I made the script to find out supported and unsupported TLS protocol versions
+by Apple's APNS servers (`gateway.sandbox.push.apple.com:2195` and
+`gateway.push.apple.com:2195`). But the script could be useful to identify TLS
+protocol versions on any other SSL/TLS servers that responds to `openssl
+s_client ...`. For better results, make sure to use a recent version of
+`openssl`.
+
+## Example Usage
+
+```
+check-tls.sh gateway.push.apple.com 2195
+
+[+] using openssl binary at: /usr/bin/openssl
+[+] openssl version: OpenSSL 0.9.8zg 14 July 2015
+[+] openssl client supports: -ssl2 -ssl3 -tls1 -dtls1
+[+] ignoring from check: dtls1
+
+[+] trying '-ssl2' on 'gateway.push.apple.com:2195'
+    - connection failed with -ssl2
+    - session master-key:
+[+] trying '-ssl3' on 'gateway.push.apple.com:2195'
+    - connection failed with -ssl3
+    - session master-key:
+[+] trying '-tls1' on 'gateway.push.apple.com:2195'
+    - connection failed with -tls1
+    - session master-key:
+```
+
+The third argument to the location of OpenSSL binary is optional. But it is
+useful to switch between OpenSSL versions available on the system.
+
+For example, at the time of writing OS X's built in OpenSSL version is
+`0.9.8zg` which apparently only speaks `SSLv2, SSLv3` and `TLSv1` protocols (no
+support for `TLSv1.1` and `TLSv1.2`).
+
+A quick usage is shown below:
+
+```
+[+] using openssl binary at: /usr/local/bin/openssl
+[+] openssl version: OpenSSL 1.0.2e 3 Dec 2015
+[+] openssl client supports: -ssl2 -ssl3 -tls1_2 -tls1_1 -tls1 -dtls1
+[+] ignoring from check: dtls1
+
+[+] trying '-ssl2' on 'gateway.push.apple.com:2195'
+    - connection failed with -ssl2
+    - session master-key:
+[+] trying '-ssl3' on 'gateway.push.apple.com:2195'
+    - connection failed with -ssl3
+    - session master-key:
+[+] trying '-tls1_2' on 'gateway.push.apple.com:2195'
+    - connection failed with -tls1_2
+    - session master-key:
+[+] trying '-tls1_1' on 'gateway.push.apple.com:2195'
+    - connection failed with -tls1_1
+    - session master-key:
+[+] trying '-tls1' on 'gateway.push.apple.com:2195'
+    - connection succesful with -tls1
+    - session master-key: 8F087AFF10895B368D5691127B6C40C65C066418CEB9CE07378FF799F0ED21BCCC6FB21654F917D28888175734EF805B
+```
---
+++ check-tls.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+DEBUG=0 ## shows debug output
+OPENSSL_BIN='' ## auto-detected
+IGNORED_PROTOCOLS='dtls1'
+
+COLOR_RESET=$(tput sgr0 2>/dev/null)
+COLOR_RED=$(tput setaf 1 2>/dev/null)
+COLOR_BLUE=$(tput setaf 4 2>/dev/null)
+
+echo -ne "${COLOR_RESET}"
+
+function platform_specific_sed() {
+    if [[ $(uname) == 'Darwin' ]]
+    then
+        echo -n 'sed -E'
+    elif [[ $(uname) == 'Linux' ]]
+    then
+        echo -n 'sed -r'
+    else
+        echo -n ''
+    fi
+}
+
+function usage() {
+    if [[ ${DEBUG} == 1 ]]
+    then
+        echo "[+] Total arguments: $#"
+    fi
+
+    if [[ $# < 2 ]]
+    then
+        echo
+        echo -e "${COLOR_RED}[+] usage: $(basename $0) host port
+        location-of-openssl-bin (optional) ${COLOR_RESET}\n"
+        echo "e.g. $(basename $0) gateway.push.apple.com 2195 /usr/bin/openssl"
+        echo
+        exit
+    fi
+}
+
+function detect_openssl_binary() {
+    local openssl_bin=$(which openssl)
+    if [[ -z ${openssl_bin} ]]
+    then
+        echo '[+] openssl binary not found, quitting ...'
+        exit -1
+    else
+        echo -n "${openssl_bin}"
+    fi
+}
+
+function detect_openssl_capability() {
+    local magic_string='just use'
+    local openssl_client_capabilities=$(${OPENSSL_BIN} s_client -help 2>&1 \
+        | grep -i "${magic_string}" \
+        | awk '{print $1}' \
+        | xargs echo)
+
+    if [[ ${#openssl_client_capabilities[*]} == 0 ]]
+    then
+        echo "[+] ${OPENSSL_BIN} has zero client capability, quitting ..."
+        exit -1
+    else
+        echo -n "${openssl_client_capabilities}"
+    fi
+}
+
+function check_server() {
+    local SERVER_HOST="$1"
+    local SERVER_PORT="$2"
+    local PROTOCOL="$3"
+    local MAGIC_STRING="Master-Key:"
+
+    local result=$(${OPENSSL_BIN} s_client \
+        -connect ${SERVER_HOST}:${SERVER_PORT} ${PROTOCOL} \
+        2>&1 </dev/null \
+        | grep "${MAGIC_STRING}")
+
+    if [[ ${DEBUG} == 1 ]]
+    then
+        ${OPENSSL_BIN} s_client \
+            -connect ${SERVER_HOST}:${SERVER_PORT} ${PROTOCOL} \
+            2>&1 </dev/null
+    fi
+
+    if [[ ! -z ${result} ]]
+    then
+        echo "$result" \
+            | $(platform_specific_sed) "s/${MAGIC_STRING}//g;s/[\t ]+//g;"
+    fi
+}
+
+function main() {
+    usage $@
+
+    if [[ -z "$3" ]]
+    then
+        OPENSSL_BIN=$(detect_openssl_binary)
+    else
+        OPENSSL_BIN="$3"
+    fi
+
+    echo "[+] using openssl binary at: ${OPENSSL_BIN}"
+    echo "[+] openssl version: $(${OPENSSL_BIN} version)"
+
+    if [[ -f ${OPENSSL_BIN} ]]
+    then
+        OPENSSL_CLIENT_CAPABILITIES=$(detect_openssl_capability)
+        echo "[+] openssl client supports: ${OPENSSL_CLIENT_CAPABILITIES}"
+    fi
+
+    if [[ ! -z ${IGNORED_PROTOCOLS} ]]
+    then
+        echo "[+] ignoring from check: ${IGNORED_PROTOCOLS}"
+    fi
+
+    echo
+    for protocol in ${OPENSSL_CLIENT_CAPABILITIES}
+    do
+        if [[ ! $(echo "${protocol}" | grep "${IGNORED_PROTOCOLS}") ]]
+        then
+            echo "[+] trying '${protocol}' on '$1:$2'"
+            local result=$(check_server "$1" "$2" "${protocol}")
+            if [[ -z "${result}" ]]
+            then
+                echo -e "${COLOR_RED}    - connection failed with ${protocol} ${COLOR_RESET}"
+            else
+                echo -e "${COLOR_BLUE}    - connection succesful with ${protocol} ${COLOR_RESET}"
+            fi
+            echo "    - session master-key: $result"
+        fi
+    done
+}
+
+main $@
+
+
